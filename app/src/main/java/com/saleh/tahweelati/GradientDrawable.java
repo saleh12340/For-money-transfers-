d0cba8b7987106664c0cc1f8731738d778b63b1c
saleh12340 <salthbiany@gmail.com>
2026-09-17 23:10:09 +0300

    إصلاح توافق واجهة Android البرمجية

---
+++ app/src/main/java/com/saleh/tahweelati/GradientDrawable.java
@@ -0,0 +1,6 @@
+package com.saleh.tahweelati;
+
+/** Small package-local alias used by the programmatic UI. */
+class GradientDrawable extends android.graphics.drawable.GradientDrawable {
+    GradientDrawable() { super(); }
+}
